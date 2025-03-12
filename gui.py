42faf43dc5e0c92ef060a458546d25d83715a283
Bannu <vidhyalakshmivaraprasadbathina@gmail.com>
2025-03-11 22:59:31 -0500

    Implemented Edit Button

--- gui.py
+++ gui.py
@@ -4,9 +4,11 @@ import FreeSimpleGUI as sg
 label = sg.Text('Type In A To-Do')
 input_box = sg.InputText(tooltip='Enter To-Do', key='todo')
 add_button = sg.Button("ADD")
-
+list_box = sg.Listbox(values = functions.get_todos(), key='todos',
+                      enable_events=True, size=[45, 10])
+edit_button = sg.Button("EDIT")
 window = sg.Window('To-Do APP', 
-                   layout=[[label], [input_box, add_button]],
+                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
                    font=('Helvetica', 20))
 
 while(True):
@@ -19,7 +21,22 @@ while(True):
             new_todo = values['todo']+"\n"
             todos.append(new_todo)
             functions.write_todos(todos_args=todos)
+            window['todos'].update(values=todos)
             """ Added ADD feature calling functions from module"""
+
+        case "EDIT":
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo']
+
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+
+        case "todos":
+            window['todo'].update(value=values['todos'][0])
+
         case sg.WIN_CLOSED:
             break
 
