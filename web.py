4a5d41a9d1937bbb9bdd9c9a986740d558ac1a97
Bannu <vidhyalakshmivaraprasadbathina@gmail.com>
2025-03-13 18:32:44 -0500

    Added Complete task with checkbox feature

--- web.py
+++ web.py
@@ -12,8 +12,13 @@ st.title("TO-DO List App")
 st.subheader("This is My TODO List App")
 st.write("This App is to manage your TODOs")
 
-for todo in todos:
-    st.checkbox(todo)
+for index,todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
 st.text_input(label='',placeholder='Add New ToDo',
               on_change=add_todo, key="new_todo")
